a8871016fe46f69a1d5dd9092f789897965720a2
MSathieu <18145111+MSathieu@users.noreply.github.com>
2017-08-23 15:29:01 +0200

    Update commit to use sha1

--- lib/commit.go
+++ lib/commit.go
@@ -1,9 +1,12 @@
 package lib
 
 import (
+	"crypto/sha1"
 	"errors"
+	"fmt"
 	"io"
 	"os"
+	"os/user"
 	"path"
 	"path/filepath"
 	"strings"
@@ -11,11 +14,22 @@ import (
 	"github.com/MSathieu/SimpleVCS/util"
 )
 
+var currentTime = util.GetTime()
+var currentUser, _ = user.Current()
+var currentPath, _ = os.Getwd()
+var message = "author " + currentUser.Username + "\ntime " + currentTime
+var sum = sha1.Sum([]byte(message))
+var commitPath = path.Join(".svcs", fmt.Sprintf("%x", sum))
+var messagePath = path.Join(commitPath, "message.txt")
+
 func Commit() error {
 	exists := util.VCSExists(".svcs")
 	if !exists {
 		return errors.New("not initialized")
 	}
+	os.Mkdir(commitPath, 0700)
+	file, _ := os.Create(messagePath)
+	file.Write([]byte(message))
 	filepath.Walk(".", visit)
 	return nil
 }
@@ -27,11 +41,8 @@ func visit(filePath string, fileInfo os.FileInfo, err error) error {
 			return nil
 		}
 	}
-	currentPath, _ := os.Getwd()
-	copyTo := path.Join(".svcs", util.GetTime())
-	os.Mkdir(copyTo, 0700)
 	relativePath := strings.Replace(fixedPath, currentPath, "", 1)
-	newPath := path.Join(copyTo, relativePath)
+	newPath := path.Join(commitPath, relativePath)
 	if fileInfo.IsDir() {
 		os.Mkdir(newPath, fileInfo.Mode())
 	} else {

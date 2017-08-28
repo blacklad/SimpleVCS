0de4d401ed0ae5f35bdc712fbdd1af4fd2bd7248
MSathieu <18145111+MSathieu@users.noreply.github.com>
2017-08-28 17:47:22 +0200

    Fetch commits, branches and tags

--- cmd/pull.go
+++ cmd/pull.go
@@ -1,5 +1,37 @@
 package cmd
 
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strings"
+)
+
 func Pull(url string) error {
+	conn, err := net.Dial("udp", url)
+	if err != nil {
+		return err
+	}
+	fmt.Fprint(conn, "commits")
+	reader := bufio.NewReader(conn)
+	message, err := reader.ReadString('\r')
+	if err != nil {
+		return err
+	}
+	commitsArr := strings.Split(message, "\n")
+	fmt.Fprint(conn, "branches")
+	reader = bufio.NewReader(conn)
+	message, err = reader.ReadString('\r')
+	if err != nil {
+		return err
+	}
+	branchesArr := strings.Split(message, "\n")
+	fmt.Fprint(conn, "tags")
+	reader = bufio.NewReader(conn)
+	message, err = reader.ReadString('\r')
+	if err != nil {
+		return err
+	}
+	tagsArr := strings.Split(message, "\n")
 	return nil
 }

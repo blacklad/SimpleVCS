2deef15b7f27c0de28d971942a5ca33b25676dd0
MSathieu <18145111+MSathieu@users.noreply.github.com>
2017-09-23 12:46:40 +0200

    Add ignore file

--- lib/ignore.go
+++ lib/ignore.go
@@ -1,10 +1,25 @@
 package lib
 
+import (
+	"io/ioutil"
+	"strings"
+)
+
 //CheckIgnored checks if the file/directory must be ignored.
 func CheckIgnored(file string) (bool, error) {
 	switch file {
 	case ".svcs", ".git", ".svn", ".hg":
 		return true, nil
+	}
+	ignoreContent, err := ioutil.ReadFile(".svcs/ignore.txt")
+	if err != nil {
+		return false, err
+	}
+	ignoreArr := strings.Split(string(ignoreContent), "\n")
+	for _, line := range ignoreArr {
+		if line == file {
+			return true, nil
+		}
 	}
 	return false, nil
 }

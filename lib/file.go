33f8563e941611426697325aa4f6e5b412ffc912
MSathieu <18145111+MSathieu@users.noreply.github.com>
2017-09-27 07:42:39 +0200

    Changes

--- lib/file.go
+++ lib/file.go
@@ -1,8 +1,32 @@
 package lib
 
+import (
+	"crypto/sha1"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"path"
+)
+
 //File is the file object.
 type File struct {
-	Name     string
-	Content  string
-	Checksum string
+	Content string
+	Hash    string
+}
+
+//GetFile gets a file.
+func GetFile(hash string) (File, error) {
+	if hash == "" {
+		return File{}, nil
+	}
+	zippedFile, err := ioutil.ReadFile(path.Join(".svcs/files", hash))
+	if err != nil {
+		return File{}, err
+	}
+	file := Unzip(string(zippedFile))
+	newSha := sha1.Sum([]byte(file))
+	if hash != fmt.Sprintf("%x", newSha) {
+		return File{}, errors.New("data has been tampered with")
+	}
+	return File{Content: file, Hash: hash}, nil
 }

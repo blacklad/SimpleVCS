af5487ffad3817566b27aa0636abf95190666c6f
MSathieu <18145111+MSathieu@users.noreply.github.com>
2017-09-27 07:38:17 +0200

    Add some types

---
+++ lib/file.go
@@ -0,0 +1,8 @@
+package lib
+
+//File is the file object.
+type File struct {
+	Name     string
+	Content  string
+	Checksum string
+}
---
+++ lib/tree.go
@@ -0,0 +1,7 @@
+package lib
+
+//Tree is the tree object.
+type Tree struct {
+	Hash  string
+	Files []File
+}

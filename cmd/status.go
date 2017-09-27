5d01363eeb4ade05ff8a974b920f3750ff1e4a18
MSathieu <18145111+MSathieu@users.noreply.github.com>
2017-09-27 19:32:29 +0200

    Add some code

--- cmd/status.go
+++ cmd/status.go
@@ -1,6 +1,34 @@
 package cmd
 
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/MSathieu/SimpleVCS/lib"
+)
+
+var currentFiles []string
+
 //Status prints the status.
 func Status() error {
+	head, err := lib.GetHead()
+	if err != nil {
+		return err
+	}
+	commit, err := lib.GetCommit(head)
+	if err != nil {
+		return err
+	}
+	files, err := commit.GetFiles()
+	if err != nil {
+		return err
+	}
+	err = filepath.Walk(".", statusVisit)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func statusVisit(filePath string, fileInfo os.FileInfo, err error) error {
 	return nil
 }

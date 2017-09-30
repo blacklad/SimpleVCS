a6115aa1d04225bbdd58cf42c3f2fd38475a59ae
MSathieu <18145111+MSathieu@users.noreply.github.com>
2017-09-30 15:05:36 +0200

    Checkout concurrently

--- cmd/checkout.go
+++ cmd/checkout.go
@@ -2,21 +2,25 @@ package cmd
 
 import (
 	"io/ioutil"
+	"log"
 	"os"
 	"path"
 	"strings"
+	"sync"
 
 	"github.com/MSathieu/SimpleVCS/lib"
 )
+
+var wait sync.WaitGroup
 
 //Checkout checks out the specified commit.
 func Checkout(commitHash string) error {
 	checkoutBranch := commitHash
 	currentCommit, isBranch, err := lib.ConvertToCommit(commitHash)
-	commitHash = currentCommit.Hash
 	if err != nil {
 		return err
 	}
+	commitHash = currentCommit.Hash
 	commit, err := lib.GetCommit(commitHash)
 	if err != nil {
 		return err
@@ -29,35 +33,9 @@ func Checkout(commitHash string) error {
 		if fileEntry == "" {
 			continue
 		}
+		wait.Add(1)
 		mapping := strings.Split(fileEntry, " ")
-		copyFrom := path.Join(".svcs/files", mapping[1])
-		fileContent, err := ioutil.ReadFile(copyFrom)
-		if err != nil {
-			return err
-		}
-		splitFileArr := strings.Split(mapping[0], "/")
-		splitFileArr = splitFileArr[:len(splitFileArr)-1]
-		toDir := ""
-		for _, element := range splitFileArr {
-			toDir = toDir + element + "/"
-		}
-		unzippedContent, err := lib.Unzip(string(fileContent))
-		if err != nil {
-			return err
-		}
-		err = lib.CheckIntegrity(unzippedContent, mapping[1])
-		if err != nil {
-			return err
-		}
-		err = os.MkdirAll(toDir, 666)
-		newFile, err := os.Create(mapping[0])
-		if err != nil {
-			return err
-		}
-		_, err = newFile.WriteString(unzippedContent)
-		if err != nil {
-			return err
-		}
+		go concProcessFile(mapping[1], mapping[0])
 	}
 	head, err := os.Create(".svcs/head.txt")
 	if err != nil {
@@ -71,5 +49,41 @@ func Checkout(commitHash string) error {
 	if err != nil {
 		return err
 	}
+	wait.Wait()
 	return nil
+}
+func concProcessFile(hash string, name string) {
+	copyFrom := path.Join(".svcs/files", hash)
+	fileContent, err := ioutil.ReadFile(copyFrom)
+	if err != nil {
+		log.Fatal(err)
+	}
+	splitFileArr := strings.Split(name, "/")
+	splitFileArr = splitFileArr[:len(splitFileArr)-1]
+	toDir := ""
+	for _, element := range splitFileArr {
+		toDir = toDir + element + "/"
+	}
+	unzippedContent, err := lib.Unzip(string(fileContent))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = lib.CheckIntegrity(unzippedContent, hash)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = os.MkdirAll(toDir, 666)
+	newFile, err := os.Create(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	newFile.WriteString(unzippedContent)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = newFile.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	wait.Done()
 }

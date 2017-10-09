e162f98b7fcae5ae48f3f1d2e96ab2680ab9ed3b
MSathieu <18145111+MSathieu@users.noreply.github.com>
2017-10-09 19:40:47 +0200

    Refactor hooks

--- lib/hooks.go
+++ lib/hooks.go
@@ -1,9 +1,24 @@
 package lib
 
-import gake "github.com/MSathieu/Gake/lib"
+import (
+	"os"
 
-//ExecHook executes a hook.
-func ExecHook(name string) error {
-	gake.Process(".svcs/hooks/"+name, true, false)
+	gake "github.com/MSathieu/Gake/lib"
+)
+
+//ExecTarget executes a Gake task.
+func ExecTarget(path string) error {
+	_, err := os.Stat(path)
+	if err != nil {
+		return nil
+	}
+	gake.Process(path, true, false)
 	return nil
+}
+
+//ExecHook provides a wrapper above ExecTarget.
+func ExecHook(name string) error {
+	path := ".svcs/hooks/" + name
+	err := ExecTarget(path)
+	return err
 }

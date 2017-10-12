1b539d08c784fc33209af8d0c729545bd6743d63
MSathieu <18145111+MSathieu@users.noreply.github.com>
2017-10-12 19:44:44 +0200

    Pull files

--- cmd/pull.go
+++ cmd/pull.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+
+	"github.com/MSathieu/SimpleVCS/lib"
 )
 
 //Pull pulls the latest changes.
@@ -20,6 +22,31 @@ func Pull(url string) error {
 	systemSplit := strings.Split(string(systemBytes), " ")
 	if systemSplit[0] != "simplevcs" {
 		return errors.New("unknown server")
+	}
+	filesResponse, err := http.Get(url + "/files")
+	if err != nil {
+		return err
+	}
+	filesBytes, err := ioutil.ReadAll(filesResponse.Body)
+	if err != nil {
+		return err
+	}
+	filesSplit := strings.Split(string(filesBytes), "\n")
+	for _, file := range filesSplit {
+		fileSplit := strings.Split(file, " ")
+		_, err := lib.GetFile(fileSplit[0])
+		if err != nil {
+			continue
+		}
+		decodedFile, err := lib.Decode(fileSplit[1])
+		if err != nil {
+			return err
+		}
+		fileObj := lib.File{Hash: fileSplit[0], Content: decodedFile}
+		err = fileObj.Save()
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }

6d4db3dc6b708f2ecdc0b732e01005b070a8056b
MSathieu <18145111+MSathieu@users.noreply.github.com>
2017-10-12 18:55:08 +0200

    Implement server checking

--- cmd/pull.go
+++ cmd/pull.go
@@ -1,6 +1,25 @@
 package cmd
 
+import (
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
 //Pull pulls the latest changes.
 func Pull(url string) error {
+	systemResponse, err := http.Get(url + "/system")
+	if err != nil {
+		return err
+	}
+	systemBytes, err := ioutil.ReadAll(systemResponse.Body)
+	if err != nil {
+		return err
+	}
+	systemSplit := strings.Split(string(systemBytes), " ")
+	if systemSplit[0] != "simplevcs" {
+		return errors.New("unknown server")
+	}
 	return nil
 }
--- cmd/push.go
+++ cmd/push.go
@@ -1,6 +1,25 @@
 package cmd
 
+import (
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
 //Push pushes the changes to the server.
 func Push(url string) error {
+	systemResponse, err := http.Get(url + "/system")
+	if err != nil {
+		return err
+	}
+	systemBytes, err := ioutil.ReadAll(systemResponse.Body)
+	if err != nil {
+		return err
+	}
+	systemSplit := strings.Split(string(systemBytes), " ")
+	if systemSplit[0] != "simplevcs" {
+		return errors.New("unknown server")
+	}
 	return nil
 }

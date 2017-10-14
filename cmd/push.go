459c08a0adace81f098a1dd3aea689e2572abd5c
MSathieu <18145111+MSathieu@users.noreply.github.com>
2017-10-14 17:57:14 +0200

    Add file pushing

--- cmd/push.go
+++ cmd/push.go
@@ -4,8 +4,14 @@ import (
 	"errors"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strings"
+
+	"github.com/MSathieu/SimpleVCS/lib"
 )
+
+var body string
 
 //Push pushes the changes to the server.
 func Push(url string) error {
@@ -21,5 +27,25 @@ func Push(url string) error {
 	if systemSplit[0] != "simplevcs" {
 		return errors.New("unknown server")
 	}
+	err = filepath.Walk(".svcs/files", visitFilesPush)
+	if err != nil {
+		return err
+	}
+	_, err = http.Post(url+"/files", "", strings.NewReader(body))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func visitFilesPush(path string, info os.FileInfo, err error) error {
+	if info.IsDir() {
+		return nil
+	}
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	unzipped, err := lib.Unzip(string(content))
+	body = body + info.Name() + " " + lib.Encode(unzipped) + "\n"
 	return nil
 }

6beacc45dd86e94e5b52dbd343a202f56ce22eb9
MSathieu <18145111+MSathieu@users.noreply.github.com>
2017-10-15 10:26:38 +0200

    Fix comment, print stats and loop trhough branches and tags.

--- cmd/stats.go
+++ cmd/stats.go
@@ -1,6 +1,32 @@
 package cmd
 
-//Stats displays the repo statistics
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/MSathieu/SimpleVCS/lib"
+)
+
+//ShowStats displays the repo statistics
 func ShowStats() error {
+	var branches, tags, commits, contributors int
+	branchesArr, err := lib.ReadBranches()
+	if err != nil {
+		return err
+	}
+	for range branchesArr {
+		branches++
+	}
+	tagsArr, err := lib.ReadTags()
+	if err != nil {
+		return err
+	}
+	for range tagsArr {
+		tags++
+	}
+	fmt.Println("branches " + strconv.Itoa(branches))
+	fmt.Println("tags " + strconv.Itoa(tags))
+	fmt.Println("commits " + strconv.Itoa(commits))
+	fmt.Println("contributors " + strconv.Itoa(contributors))
 	return nil
 }

9fc95796c21e15dc10101477c83d4d949fb320c2
MSathieu <18145111+MSathieu@users.noreply.github.com>
2017-10-15 19:30:53 +0200

    Simplify ignore

--- lib/ignore.go
+++ lib/ignore.go
@@ -10,24 +10,17 @@ var ignoreList = []string{".svcs", ".git", ".svn", ".hg", "*.o", "*.exe", "*.log
 
 //CheckIgnored checks if the file/directory must be ignored.
 func CheckIgnored(file string) (bool, error) {
-	for _, value := range ignoreList {
-		match, err := path.Match(value, file)
-		if err != nil {
-			return false, err
-		}
-		if match {
-			return true, nil
-		}
-	}
 	ignoreContentBytes, err := ioutil.ReadFile(".svcsignore")
-	if err != nil {
-		return false, nil
+	if err == nil {
+		ignoreContent := string(ignoreContentBytes)
+		ignoreContent = strings.Replace(ignoreContent, "\r\n", "\n", -1)
+		ignoreContent = strings.Replace(ignoreContent, "\r", "\n", -1)
+		ignoreArr := strings.Split(ignoreContent, "\n")
+		for _, line := range ignoreArr {
+			ignoreList = append(ignoreList, line)
+		}
 	}
-	ignoreContent := string(ignoreContentBytes)
-	ignoreContent = strings.Replace(ignoreContent, "\r\n", "\n", -1)
-	ignoreContent = strings.Replace(ignoreContent, "\r", "\n", -1)
-	ignoreArr := strings.Split(ignoreContent, "\n")
-	for _, line := range ignoreArr {
+	for _, line := range ignoreList {
 		match, err := path.Match(line, file)
 		if err != nil {
 			return false, err

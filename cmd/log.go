89cd777fc537f88e19af8a3da621f2f1a8235cd4
MSathieu <18145111+MSathieu@users.noreply.github.com>
2017-11-18 08:40:12 +0100

    Improve log

--- cmd/log.go
+++ cmd/log.go
@@ -8,13 +8,13 @@ import (
 
 //Log logs all commits.
 func Log(branch string) error {
-	var commits []string
+	var commits []lib.Commit
 	lastCommit, _, err := lib.ConvertToCommit(branch)
 	if err != nil {
 		return err
 	}
 	for currentCommit := lastCommit; true; {
-		commits = append(commits, currentCommit.Hash)
+		commits = append(commits, currentCommit)
 		currentCommit, err = lib.GetCommit(currentCommit.Parent)
 		if err != nil {
 			return err
@@ -27,13 +27,9 @@ func Log(branch string) error {
 	for i := 0; i < len(commits)/2; i++ {
 		commits[i], commits[last-i] = commits[last-i], commits[i]
 	}
-	for _, sha := range commits {
-		fmt.Println(sha)
-		commit, err := lib.GetCommit(sha)
-		if err != nil {
-			return err
-		}
-		fmt.Println(commit.Message + "\n")
+	for _, commit := range commits {
+		fmt.Println(commit.Hash + " " + commit.Author + " " + commit.Time)
+		fmt.Println(commit.Message)
 	}
 	return nil
 }

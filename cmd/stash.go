e82621c47f96a5baa3d520b5e5e0f7ef11ae2411
MSathieu <18145111+MSathieu@users.noreply.github.com>
2017-11-26 06:08:55 +0100

    Add stash create

--- cmd/stash.go
+++ cmd/stash.go
@@ -1,10 +1,32 @@
 package cmd
 
-import "os"
+import (
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/MSathieu/SimpleVCS/util"
+)
 
 //CreateStash creates a stash
 func CreateStash(name string) error {
-	return nil
+	err := filepath.Walk(".", commitVisit)
+	if err != nil {
+		return err
+	}
+	commitWait.Wait()
+	stashFile, err := os.Create(".svcs/stashes/" + name)
+	if err != nil {
+		return err
+	}
+	defer stashFile.Close()
+	stashFileContent := strings.Join(filesStruct.files, "\n")
+	stashFileContent, err = util.Zip(stashFileContent)
+	if err != nil {
+		return err
+	}
+	_, err = stashFile.WriteString(stashFileContent)
+	return err
 }
 
 //CheckoutStash checkouts a stash

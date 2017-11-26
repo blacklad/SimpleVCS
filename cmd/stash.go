487de2f0056b73c3e413eb47e9ec92270a45dda3
MSathieu <18145111+MSathieu@users.noreply.github.com>
2017-11-26 06:31:16 +0100

    Add stash checkout

--- cmd/stash.go
+++ cmd/stash.go
@@ -1,12 +1,20 @@
 package cmd
 
 import (
+	"io/ioutil"
+	"log"
 	"os"
+	"path"
 	"path/filepath"
 	"strings"
+	"sync"
+
+	"github.com/MSathieu/Gotils"
 
 	"github.com/MSathieu/SimpleVCS/util"
 )
+
+var wait sync.WaitGroup
 
 //CreateStash creates a stash
 func CreateStash(name string) error {
@@ -31,7 +39,54 @@ func CreateStash(name string) error {
 
 //CheckoutStash checkouts a stash
 func CheckoutStash(name string) error {
+	stash, err := ioutil.ReadFile(".svcs/stashes/" + name)
+	if err != nil {
+		return err
+	}
+	stashContent, err := util.Unzip(string(stash))
+	if err != nil {
+		return err
+	}
+	stashArr := strings.Split(stashContent, "\n")
+	for _, fileEntry := range stashArr {
+		if fileEntry == "" {
+			continue
+		}
+		wait.Add(1)
+		mapping := strings.Split(fileEntry, " ")
+		go concProcessFile(mapping[1], mapping[0])
+	}
+	wait.Wait()
 	return nil
+}
+func concProcessFile(hash string, name string) {
+	copyFrom := path.Join(".svcs/files", hash)
+	fileContent, err := ioutil.ReadFile(copyFrom)
+	if err != nil {
+		log.Fatal(err)
+	}
+	unzippedContent, err := util.Unzip(string(fileContent))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = gotils.CheckIntegrity(unzippedContent, hash)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = os.MkdirAll(filepath.Dir(name), 666)
+	newFile, err := os.Create(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	newFile.WriteString(unzippedContent)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = newFile.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	wait.Done()
 }
 
 //RemoveStash removes a stash
